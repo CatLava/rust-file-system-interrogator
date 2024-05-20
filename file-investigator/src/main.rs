0f1751112d4c829555c584629830b1b4682f1ba7
Evan Schoening <evan.schoening@gmail.com>
2024-05-20 15:03:07 -0700

    adding file inspection and run

--- file-investigator/src/main.rs
+++ file-investigator/src/main.rs
@@ -31,3 +31,38 @@ fn main() {
         }
     }
 }
+
+// Inspect a directory, if file is directory, add to vec and return vec 
+pub fn inspect_dir(file_path: str) -> Vec[str] {
+    let mut found_dirs: Vec<str> = vec![]
+    match fs::read_dir(file_path) {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(entry) => {
+                        let path = entry.path();
+                        println!("{:?}", path);
+                        let metadata = fs::metadata(path);
+                        match metadata {
+                            Ok(metadata) => {println!("{:?}", metadata.file_type());
+                                            println!("is dir {:?}", metadata.is_dir());
+                                            if metadata.is_dir() {
+                                                found_dirs.append(path)
+                                            }
+                                            },
+                            Err(err) => {
+                                eprintln!("Error: {}", err);
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("Error: {}", err);
+                    }
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        }
+    }
+}
